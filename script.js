7d2fdf1a7e87f3b246801f4a823dc84d8ecf94ab
Michael Eisenbraun <eisenbm@algonquincollege.com>
2024-04-01 10:17:28 -0400

    Creates final version

--- script.js
+++ script.js
@@ -4,16 +4,57 @@ const $more = document.getElementById('more')
 const $dialog = document.getElementById('dialog')
 
 // Data
-
+const deck = []
 
 // Functions
+function displayProperties (properties) {
+  deck.push(...properties)
+  $properties.innerHTML = deck.reduce((html, prop) => html + `
+  <div class="property" data-id="${prop.id}">
+    <div class="property-group" style="background-color: ${prop.group}"></div>
+    <h2 class="property-title">${prop.name}</h2>
+    <p class="property-price">Price: $${prop.price}</p>
+  </div>
+  `, '')
+}
 
+function displayDeed (property) {
+  $dialog.innerHTML = `
+  <div class="deed">
+    <div style="background-color: ${property.group}">
+      <h2 class="deed-title">${property.name}</h2>
+    </div>
+    <div class="deed-rent">Rent $${property.rent}.</div>
+    <div class="deed-improved-rents">
+      ${property.improved_rents.reduce((html, item) => html + `
+        <p class="deed-improvement">${item.improvement}</p>
+        <p class="deed-rent">${item.rent}.</p>
+      `, '')}
+    </div>
+  </div>`
+}
 
 // Listeners
+$more.addEventListener('click', async function (e) {
+  const response = await fetch('https://monopoly.zoodinkers.com/api/properties?offset=' + deck.length)
+  const properties = await response.json()
+  displayProperties(properties)
+})
+
 $properties.addEventListener('click', async function (e) {
-  $dialog.showModal()
+  const $property = e.target.closest('.property')
+  if ($property) {
+    const response = await fetch('https://monopoly.zoodinkers.com/api/properties/' + $property.dataset.id)
+    const property = await response.json()
+    displayDeed(property)
+    $dialog.showModal() 
+  }
 })
 
 $dialog.addEventListener('click', function () {
   $dialog.close();
 })
+
+fetch('https://monopoly.zoodinkers.com/api/properties')
+  .then(response => response.json())
+  .then(displayProperties)
